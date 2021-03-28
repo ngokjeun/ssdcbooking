d84678c0c7cebbe7c38e199f9e66a2abb13bdf5b
ngokjeun <n.jeunw@gmail.com>
2021-03-28 09:48:38 +0800

    environ vars for user and pass, cleaner

--- ssdcbooking.py
+++ ssdcbooking.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
-#from selenium.webdriver.support.ui import WebDriverWait
-import time
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from pynotifier import Notification
+import time
+import os
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+PATH = r"C:\Users\ngokj\Desktop\ssdc2\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.ssdcl.com.sg/User/Login")
 
-user_input = driver.find_element_by_id('UserName')
-user_input.send_keys('xxx')
 
-password_input = driver.find_element_by_id('Password')
-password_input.send_keys('xxx')
+ssdc_user = driver.find_element_by_id('UserName')
+ssdc_user.send_keys(os.environ.get('ssdc_user'))
 
-login_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/form/div[5]/div/button')
-login_button.click()
+ssdc_password = driver.find_element_by_id('Password')
+ssdc_password.send_keys(os.environ.get('ssdc_password'))
 
+login_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/form/div[5]/div/button').click()
 print("LOGGED IN")
 
 booking_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/ul/li[3]/a').click()
@@ -27,15 +27,6 @@ checkproceed_button = driver.find_element_by_id('chkProceed').click()
 
 proceed_button = driver.find_element_by_id('lnkProceed').click()
 
-#extra-------------------------------------------------------------------------------------------------------------
-#earliestdate_button = driver.find_element_by_id('button-searchDate').click()
-#time.sleep(1)
-#selecteddate_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/form/div[4]/div/input')
-#print(selecteddate_button.get_attribute('value'))
-
-#time.sleep(1)
-#extra-------------------------------------------------------------------------------------------------------------
-
 availability_button = driver.find_element_by_id('btn_checkforava').click()
 
 time.sleep(2)
@@ -43,17 +34,20 @@ time.sleep(2)
 buyButton = False
 
 while not buyButton:
-        try:
-            cartout = addButton = driver.find_element_by_class_name('slotBooking')
-            print('clicked hurry')
-            cartout.click()
-            continue
+    try:
+        cartout = driver.find_element_by_class_name('slotBooking')
+        print('clicked hurry')
+        cartout.click()
+        Notification(
+            title='ssdc slot found',
+            description='hurry pay',
+            duration= 20,
+            urgency=Notification.URGENCY_CRITICAL
+        ).send()
+        time.sleep(90)
 
-
-        except (NoSuchElementException) as e:
-            print('no slots:(')
-            driver.execute_script("location.reload()")
-            time.sleep(60)
-            continue
-
+    except (NoSuchElementException) as e:
+        print('no slots:(')
+        driver.execute_script('location.reload()')
+        time.sleep(90)
 
