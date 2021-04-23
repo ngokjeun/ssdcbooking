8bf8122de7104edf4c9db5788ce25121c46906af
ngokjeun <n.jeunw@gmail.com>
2021-04-23 22:26:40 +0800

    Update ssdcbooking.py

--- ssdcbooking.py
+++ ssdcbooking.py
@@ -8,46 +8,48 @@ import requests
 PATH = r"C:\Users\ngokj\Desktop\ssdc2\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.ssdcl.com.sg/User/Login")
+def navig():
+    driver.get("https://www.ssdcl.com.sg/User/Login")
+    ssdc_user = driver.find_element_by_id('UserName')
+    ssdc_user.send_keys(os.environ.get('ssdc_user'))
+    ssdc_password = driver.find_element_by_id('Password')
+    ssdc_password.send_keys(os.environ.get('ssdc_password'))
+    driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/form/div[5]/div/button').click() #login 
+    print('logged in')
+    driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/ul/li[3]/a').click() #booking 
+    driver.find_element_by_id('btnNewBooking').click() #new booking
+    driver.find_element_by_id('chkProceed').click() #check proceed
+    driver.find_element_by_id('lnkProceed').click() #proceed
+    driver.find_element_by_id('btn_checkforava').click() #availability
+    time.sleep(2)
 
-ssdc_user = driver.find_element_by_id('UserName')
-ssdc_user.send_keys(os.environ.get('ssdc_user'))
+def clickslot():
+    buyButton = False
+    while not buyButton:
+        try:
+        #book first slot within the week
+            #cartout = driver.find_element_by_class_name('slotBooking')
+        #book specific date
+            cartout = driver.find_element_by_xpath('//a[@class="slotBooking"][contains(@id, "25/7/2021")]')
+            print('clicked, pay now')
+            cartout.click()
+            Notification(
+                title='ssdc slot found',
+                description='hurry pay',
+                duration= 30,
+                urgency=Notification.URGENCY_CRITICAL
+            ).send()
+            #ifttt integration with maker, connect own ifttt account
+            requests.get('https://maker.ifttt.com/trigger/clicked/with/key/ddg9gyPPaIJ-1sJWzMpsI_')
+            time.sleep(60)
 
-ssdc_password = driver.find_element_by_id('Password')
-ssdc_password.send_keys(os.environ.get('ssdc_password'))
+        except (NoSuchElementException):
+            print('no slots:(')
+            driver.execute_script('location.reload()')
+            time.sleep(60)
 
-login_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/div/div/form/div[5]/div/button').click()
-print("LOGGED IN")
+def main():
+    navig()
+    clickslot()
 
-booking_button = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[2]/ul/li[3]/a').click()
-
-newbooking_button = driver.find_element_by_id('btnNewBooking').click()
-
-checkproceed_button = driver.find_element_by_id('chkProceed').click()
-
-proceed_button = driver.find_element_by_id('lnkProceed').click()
-
-availability_button = driver.find_element_by_id('btn_checkforava').click()
-
-time.sleep(2)
-
-buyButton = False
-while not buyButton:
-    try:
-        cartout = driver.find_element_by_class_name('slotBooking')
-        print('clicked hurry')
-        cartout.click()
-        Notification(
-            title='ssdc slot found',
-            description='hurry pay',
-            duration= 30,
-            urgency=Notification.URGENCY_CRITICAL
-        ).send()
-        requests.get('https://maker.ifttt.com/trigger/clicked/with/key/ddg9gyPPaIJ-1sJWzMpsI_')
-        time.sleep(60)
-
-    except (NoSuchElementException) as e:
-        print('no slots:(')
-        driver.execute_script('location.reload()')
-        time.sleep(90)
-
+main()
